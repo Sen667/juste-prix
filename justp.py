c0966a06e574018120e3372b873a060eaf042eab
Sen <81926645+Sen667@users.noreply.github.com>
2022-12-21 01:43:04 +0100

    Add files via upload

---
+++ justp.py
@@ -0,0 +1,13 @@
+print ("bienvenue au juste prix", end=" ") ; print ("indiquer une valeur !")
+print ("x--------------x vous avez 120 essaies pour trouver le bon prix ! x--------------x")
+prix = 34651564787632124
+
+for loop in range(12):
+ value = int(input()) 
+ if  prix < value:
+        print("le prix est trop haut bro ")
+ if prix > value:
+    print("prix trop bas bro ")
+
+ if  prix == value:
+    print("wow ta gagné") ; print("la partie est terminée") 
