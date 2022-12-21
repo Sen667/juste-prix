6f61dbb4bf34272990677c942870abcc1ddd0776
Sen <81926645+Sen667@users.noreply.github.com>
2022-12-21 01:43:36 +0100

    Update justp.py

--- justp.py
+++ justp.py
@@ -2,7 +2,7 @@ print ("bienvenue au juste prix", end=" ") ; print ("indiquer une valeur !")
 print ("x--------------x vous avez 120 essaies pour trouver le bon prix ! x--------------x")
 prix = 34651564787632124
 
-for loop in range(12):
+for loop in range(120):
  value = int(input()) 
  if  prix < value:
         print("le prix est trop haut bro ")
