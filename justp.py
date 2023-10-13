b001f3de533575978454fd43e753451f7c886285
Sen <81926645+Sen667@users.noreply.github.com>
2023-10-14 01:28:07 +0200

    Update juste_prix.py
    - Fixing errors 
    + Addition of a number generator
    + Added a character number selector
    + Addition of a retry function

--- justp.py
+++ justp.py
@@ -1,13 +1,31 @@
-print ("bienvenue au juste prix", end=" ") ; print ("indiquer une valeur !")
-print ("x--------------x vous avez 120 essaies pour trouver le bon prix ! x--------------x")
-prix = 34651564787632124
+import random
 
-for loop in range(120):
- value = int(input()) 
- if  prix < value:
-        print("le prix est trop haut bro ")
- if prix > value:
-    print("prix trop bas bro ")
+while True:
+    print("Bienvenue au Juste Prix")
+    longueur = int(input("Indiquez la longueur du nombre : "))
+    essaies = int(input("Combien d'essais voulez-vous ?"))
 
- if  prix == value:
-    print("wow ta gagné") ; print("la partie est terminée") 
+    # Génération d'un prix aléatoire avec la longueur spécifiée
+    prix = random.randint(10 ** (longueur - 1), (10 ** longueur) - 1)
+
+    print("x--------------x Vous avez", essaies, "essais pour trouver le bon prix ! x--------------x")
+
+    for loop in range(essaies):
+        try:
+            value = int(input("Essai " + str(loop + 1) + ": "))
+            if prix < value:
+                print("Le prix est trop haut, essayez à nouveau.")
+            elif prix > value:
+                print("Le prix est trop bas, essayez à nouveau.")
+            else:
+                print("Bravo, vous avez gagné ! La partie est terminée.")
+                break
+        except ValueError:
+            print("Veuillez entrer un nombre valide.")
+
+    if loop == essaies - 1:
+        print("Désolé, vous avez épuisé vos essais. La partie est terminée. Le prix était", prix)
+
+    replay = input("Voulez-vous rejouer ? (Oui/Non) ").lower()
+    if replay != "oui":
+        break
